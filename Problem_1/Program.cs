f09f3a36ee2aa571f0703cd91605aad95e0baa6d
Rafael Pazos <rarod@microsoft.com>
2021-01-06 10:36:47 +0100

    problem 1 solved!

--- Problem_1/Program.cs
+++ Problem_1/Program.cs
@@ -3,7 +3,16 @@
 namespace Euler.Problem_1 {
    class Program {
       static void Main(string[] args) {
-         Console.WriteLine("Hello World!");
+         Console.WriteLine("Euler Problem 1");
+         double res = mcp1(3.0, 999.0) + mcp1(5.0, 999.0) - mcp1(15.0, 999.0);
+         Console.WriteLine(res);
+         Console.ReadKey();
+         return;
+      }
+
+      static double mcp1(double n, double m) {
+         double fl = Math.Floor(m/n);
+         return n*fl*(fl+1)/2;
       }
    }
 }
