af8431485a6c325f00c4fe0beb521fc5e6035889
RafaPazos <rafael.pazos@microsoft.com>
2023-12-08 13:06:22 +0100

    p2

---
+++ Problem_2/src/main.rs
@@ -0,0 +1,9 @@
+// Main function for Problem 2
+
+pub mod p2;
+
+fn main() {
+    let result = p2::solve();
+    println!("Welcome to Problem 2!");
+    println!("The solution is: {}", result);
+}
---
+++ Problem_2/src/p2.rs
@@ -0,0 +1,5 @@
+// Here holds the solution for problem 2 of Project Euler
+
+pub fn solve() -> i32{
+    return 33;
+}
